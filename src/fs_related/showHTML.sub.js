4b5edc33728477d0debccc12b4a0976d40066ef6
Jan Andrle <andrle.jan@centrum.cz>
2021-12-04 11:24:41 +0100

    :bug:

--- src/fs_related/showHTML.sub.js
+++ src/fs_related/showHTML.sub.js
@@ -2,7 +2,7 @@ gulp_place("./filePathExpanded.sub.js", "file_once");/* global filePathExpanded 
 gulp_place("./fs.sub.js", "file_once");/* global existsSync, readFileSync */
 function showHTML({ param: file }){
     file= filePathExpanded(file);
-    if(!existsSync(file)) return "";
+    if(!existsSync(file)) return [""];
     let out= new Set();
     const is_jaaCSS= c=> c.indexOf("__")!==-1;
     const append= c=> out.add(c);
